c06f2b98ef553957f0cd46742217408cd862f39e
Theodor Malm <thma1.game@gmail.com>
2023-12-12 12:55:23 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,30 @@
+document.addEventListener("DOMContentLoaded", function () {
+  // Hitta elementen
+  const navbar = document.getElementById("navbar");
+  const heroSection = document.getElementById("hero-section");
+  const imageSection = document.getElementById("image-section");
+
+
+  function showElement(element) {
+    element.classList.add("visible");
+  }
+
+
+  setTimeout(function () {
+    showElement(navbar);
+  }, 500);
+
+  setTimeout(function () {
+    showElement(heroSection);
+  }, 1000);
+
+  setTimeout(function () {
+    showElement(imageSection);
+  }, 1500);
+});
+
+
+
+
+
+<script src="script.js"></script>
